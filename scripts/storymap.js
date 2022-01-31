5c2f9bb20fb628e8ad2176a5371bd1f04a4c7a0e
HesperusArts <76931696+HesperusArts@users.noreply.github.com>
2022-01-31 15:20:35 -0800

    Update storymap.js
    Line 156 replaced chapterCount with Fred

--- scripts/storymap.js
+++ scripts/storymap.js
@@ -153,7 +153,7 @@ $(window).on('load', function() {
           L.marker([lat, lon], {
             icon: L.ExtraMarkers.icon({
               icon: 'fa-number',
-              number: c['Marker'] === 'Plain' ? '' : chapterCount,
+              number: c['Marker'] === 'Plain' ? '' : 'Fred',
               markerColor: c['Marker Color'] || 'blue'
             }),
             opacity: c['Marker'] === 'Hidden' ? 0 : 0.9,
